2098ee0209ed7cfa2ed27c34a70a499e3e80cd18
Erik <ebobinski0@gmail.com>
2024-08-19 14:08:22 -0400

    mutable agent media

--- src/app/agent-generation/agent-generation.component.ts
+++ src/app/agent-generation/agent-generation.component.ts
@@ -12,10 +12,16 @@ import { CommonModule } from '@angular/common';
 export class AgentGenerationComponent {
   @ViewChild('videoPlayer') videoPlayer: ElementRef<HTMLVideoElement> | undefined;
   
-  selectedAgent: { name: string, videoUrl: string } | null;
+  selectedAgent: { name: string, videoUrl: string } | null; // holds current agent and respective video src path
+
+  toggleMuteSrc: string; // holds src path to respective image
+  unmuted = 'assets/images/audioOn.svg';
+  muted = 'assets/images/audioOff.svg';
+  isMuted: boolean = false; // video is initialized to unmuted
 
   constructor(private agentService: AgentService) {
     this.selectedAgent = null;
+    this.toggleMuteSrc = this.unmuted;
   }
 
   getRandomAgent(): void {
@@ -28,6 +34,20 @@ export class AgentGenerationComponent {
         this.videoPlayer.nativeElement.play(); // play video again
       }
     }, 0);
+  }
+
+  toggleMute(): void {
+    this.isMuted = !this.isMuted; // toggle muted state
+
+    if (this.isMuted == true) { 
+      this.toggleMuteSrc = this.muted; // load muted img path
+    }
+    else if(this.isMuted == false) { 
+      this.toggleMuteSrc = this.unmuted; // load unmuted img path
+    }
+    else {
+      console.log("toggleMute() logic error occurred");
+    }
   }
 
 }
